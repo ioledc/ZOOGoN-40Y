d54a31dbe31d4f079909b58aed4e28c981f273e6
andremarcucci24-rgb <andrmarcucci24@gmail.com>
2026-03-12 10:17:47 +0100

    workflow

--- R/preprocessing-surveys.R
+++ R/preprocessing-surveys.R
@@ -43,7 +43,6 @@ preprocess_surveys <- function(raw_data = NULL) {
     dplyr::rename_with(~ stringr::str_remove(., "group_cruise/")) |>
     dplyr::rename_with(~ stringr::str_remove(., "group_abundance/")) |>
     dplyr::rename_with(~ stringr::str_remove(., "group_sample/")) |>
-    dplyr::rename(filtered_volume_m3 = "Filtered_Volume_m") |>
     dplyr::rename(dplyr::any_of(c(filtered_volume_m3 = "Filtered_Volume_m"))) |>
     #janitor::clean_names() |>
     dplyr::relocate(
